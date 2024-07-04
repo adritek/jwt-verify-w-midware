d3f1a8c16d9cedd075625075e9c0c3f6ff7f1bac
Adrian Rogers <adritek@gmail.com>
2024-07-04 17:25:03 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+
+const app = express();
+const secretKey = 'somethingsomething';
+
+app.use(express.json());
+
+const users = [];
+
+app.post('/register', (req, res) => {
+    const { username, password } = req.body;
+
+    //check user name already exists
+    const existingUser = users.find((u) => u.username === username);
+    if (existingUser){
+        return res.status(400).json({ message: 'Username already exists' });
+    }
+
+    //add new user to the database
+    const newUser = {
+        id: users.length +1,
+        username,
+        password,
+    }
+    users.push(newUser);
+
+    res.status(201).json({ message: 'User registered successfully' });
+});
+
+// Endpoint for user login
+app.post('/login', (req, res) => {
+    const {username, password} = req.body;
+    const user = users.find((u) => u.username === username && u.password === password);
+
+    if (user) {
+        const token = jwt.sign({ id: user.id, username: user.username }, secretKey);
+        res.json({ token });
+    } else {
+        res.status(401).json({ message: 'Invalid credentials' });
+    }
+});
+
+// Protected route example (Dashboard access)
+app.get('/dashboard', (req, res) => {
+    res.json({ message: 'Welcome to the Customer Portal!' })
+});
+
+// Middleware to verify the token
+function verifyToken(req, res, next){
+    const token = req.headers['authoorization'];
+
+    if (typeof token !== undefined){
+        jwt.verify(token, secretKey, (err, authData) => {
+            if (err){
+                res.sendStatus(403);
+
+            } else {
+                req.authData = authData;
+                next();
+            }
+        });
+    } else {
+        res.sendStatus(401);
+    }
+}
+
+// Start server
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
